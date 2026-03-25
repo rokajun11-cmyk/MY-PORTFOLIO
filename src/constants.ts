f62a98cab54676875b568589a3d353218a0cdae9
rokajun <rokajun11@gmail.com>
2026-03-25 11:26:22 +0900

    m

--- src/constants.ts
+++ src/constants.ts
@@ -38,7 +38,7 @@ import b4 from '../B4.png';
 import z0 from '../z0.png';
 import z1 from '../z1.png';
 import z2 from '../z2.jpg';
-import z3 from '../z3.png';
+import z3 from '../z3.jpg';
 import m0 from '../m0.png';
 import m1 from '../m1.png';
 import m3 from '../m3.jpg';
